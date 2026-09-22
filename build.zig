5277010401ac5249067a8fdaad4103168b064453
Rishitha Minol <rishithaminol@gmail.com>
2026-09-22 15:01:45 +0530

    fix: added testing mechanism

--- build.zig
+++ build.zig
@@ -93,4 +93,24 @@ pub fn build(b: *std.Build) void {
     const run_cmd = b.addRunArtifact(exe);
     run_step.dependOn(&run_cmd.step);
     run_cmd.step.dependOn(b.getInstallStep());
+
+
+    // =============================[Testing]==============================
+    // We should define the step identifier before configuring the step
+    const test_step = b.step("test", "Run unit tests");
+    const exe_unit_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+            .imports = &.{
+                .{ .name = "zig_pcre", .module = mod },
+            },
+        }),
+        .name = "zig_pcre_test"
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    test_step.dependOn(&run_exe_unit_tests.step);
 }
